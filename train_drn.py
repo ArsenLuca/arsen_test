ef3a7fb3d158cde36eed8e07159d9cb2b6ae1e77
ArsenLuca <arsenzhang@163.com>
2017-06-30 11:33:55 +0800

    fix bugs

--- train_drn.py
+++ train_drn.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
                     "momentum": 0.9,  "wd": 1e-4, "batch_size": 256, \
                     "check_each_epoch": 10, "num_epoch": 120, \
                     "metric": 'acc', "fixed_param_names": None
-                   })
+                    })
 
     log_file = get_file_name_by_time(prefix="./logs/", ext=".log")
     result_folder = "./result"
@@ -40,4 +40,4 @@ if __name__ == "__main__":
     devs = [mx.gpu(i) for i in range(4)]
 
     mx_train(log_file, len(open(train_lst_file).readlines()), drn_sym, None, None, \
-            train_dataiter, val_dataiter, devs, os.path.join(result_folder, drn_18_imagenet), train_params)
+            train_dataiter, val_dataiter, devs, os.path.join(result_folder, "drn_18_imagenet"), train_params)
