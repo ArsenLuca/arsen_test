79c9fe71c3eafc6c27e4713dcad80b1dfbf8221d
ArsenLuca <arsenzhang@163.com>
2017-07-05 17:15:47 +0800

    fix bugs

--- arsen_toolbox/mx_tools/mx_dataiter.py
+++ arsen_toolbox/mx_tools/mx_dataiter.py
@@ -16,7 +16,14 @@ def get_dataiter(train_rec_file, val_rec_file, batch_size=128, data_shape=(3, 22
         shuffle             = True,
         rand_crop           = True,
         rand_mirror         = True,
-        mean_img            = mean_file)
+        max_random_scale    = 1.0,  # 480 with imagnet
+        min_random_scale    = 0.533,  # 256.0/480.0
+        max_aspect_ratio    = 0.25,
+        random_h            = 36,  # 0.4*90
+        random_s            = 50,  # 0.4*127
+        random_l            = 50  # 0.4*127
+        #mean_img            = mean_file
+        )
 
     val_dataiter = mx.io.ImageRecordIter(
         path_imgrec         = val_rec_file,
@@ -25,7 +32,8 @@ def get_dataiter(train_rec_file, val_rec_file, batch_size=128, data_shape=(3, 22
         batch_size          = batch_size,
         data_shape          = data_shape,
         rand_crop           = False,
-        rand_mirror         = False,
-        mean_img            = mean_file)
+        rand_mirror         = False
+        #mean_img            = mean_file
+        )
     
     return (train_dataiter, val_dataiter)
